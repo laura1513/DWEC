b4c532f4533dba241b1b62e5ddf2e784d0944a7b
laura1513 <laurajorques2003@gmail.com>
2023-12-19 09:57:07 +0100

    dom 5

---
+++ dom/dom4/script.js
@@ -0,0 +1,8 @@
+function cambiarImagen() {
+    var imagen = document.getElementById('imagen');
+    if (imagen.src.endsWith("casauno.jpeg")) {
+        imagen.src = "casados.jpeg";
+    } else {
+        imagen.src = "casauno.jpeg";
+    }
+}
