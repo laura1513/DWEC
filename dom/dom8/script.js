635d0acb8b8b65b00bef3ba4984869c9de482de7
laura1513 <laurajorques2003@gmail.com>
2023-12-21 09:32:22 +0100

    dom 8

---
+++ dom/dom8/script.js
@@ -0,0 +1,12 @@
+function verde() {
+    var c = document.getElementById('body');
+    c.style.backgroundColor = 'green';
+}
+function azul() {
+    var c = document.getElementById('body');
+    c.style.backgroundColor = 'blue';
+}
+function rojo() {
+    var c = document.getElementById('body');
+    c.style.backgroundColor = 'red';
+}
