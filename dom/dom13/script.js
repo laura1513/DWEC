d3b54a6f09cfc74146d312bc6ba83f0a5203218f
laura1513 <laurajorques2003@gmail.com>
2024-01-09 10:05:30 +0100

    dom 13

---
+++ dom/dom13/script.js
@@ -0,0 +1,15 @@
+function rojo() {
+    var f = document.getElementById('text');
+    f.innerHTML = "Bienvenido a esta página de prueba de colores";
+    f.style.backgroundColor = 'red';
+}
+function verde() {
+    var f = document.getElementById('text');
+    f.innerHTML = "Bienvenido a esta página de prueba de colores";
+    f.style.backgroundColor = 'green';
+}
+function azul() {
+    var f = document.getElementById('text');
+    f.innerHTML = "Bienvenido a esta página de prueba de colores";
+    f.style.backgroundColor = 'blue';
+}
