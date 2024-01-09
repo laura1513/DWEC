beaf03ac55acd877e3fc873a6001e11cf75c8083
laura1513 <laurajorques2003@gmail.com>
2024-01-09 10:12:40 +0100

    dom 14

---
+++ dom/dom14/script.js
@@ -0,0 +1,8 @@
+function aparecer() {
+    var f = document.getElementById('foto');
+    f.style.opacity = 100;
+}
+function desaparecer() {
+    var f = document.getElementById('foto');
+    f.style.opacity = 0;
+}
