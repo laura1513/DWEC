31c1aed1074c356f4b3056a4a2201604409c88b6
laura1513 <laurajorques2003@gmail.com>
2024-01-09 09:01:27 +0100

    dom 11

---
+++ dom/dom11/script.js
@@ -0,0 +1,8 @@
+function error(par) {
+    var f = document.getElementById(par);
+    f.style.backgroundColor = 'lightcoral';
+}
+function correcto(par) {
+    var f = document.getElementById(par);
+    f.style.backgroundColor = 'lightgreen';
+}
