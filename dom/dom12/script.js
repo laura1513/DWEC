ff4f85ea7b34edafad03f4915deacd1ae68bcdb2
laura1513 <laurajorques2003@gmail.com>
2024-01-09 10:00:00 +0100

    dom 12

---
+++ dom/dom12/script.js
@@ -0,0 +1,15 @@
+function ruso() {
+    var f = document.getElementById('text');
+    f.innerHTML = "ДОБРО ПОЖАЛОВАТЬ";
+    f.style.color = 'red';
+}
+function esp() {
+    var f = document.getElementById('text');
+    f.innerHTML = "BIENVENIDO";
+    f.style.color = 'green';
+}
+function ing() {
+    var f = document.getElementById('text');
+    f.innerHTML = "WELCOME";
+    f.style.color = 'blue';
+}
