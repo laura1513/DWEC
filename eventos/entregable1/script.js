092f207fed8078e787ddef17e9e35780aff97ed6
laura1513 <laurajorques2003@gmail.com>
2024-01-12 10:52:13 +0100

    entregable 1

--- eventos/entregable1/script.js
+++ eventos/entregable1/script.js
@@ -2,7 +2,3 @@ function aumentar() {
     var e = document.getElementById('div1');
     e.style.fontSize = 16 + "px"
 }
-function original() {
-    var e = document.getElementById('div1');
-    e.style.fontSize = 12 + "px"
-}
