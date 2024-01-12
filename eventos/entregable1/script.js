799cb7237984240bc57727dd09523d2ee0fc0470
laura1513 <laurajorques2003@gmail.com>
2024-01-12 10:51:34 +0100

    entregabe 1

---
+++ eventos/entregable1/script.js
@@ -0,0 +1,8 @@
+function aumentar() {
+    var e = document.getElementById('div1');
+    e.style.fontSize = 16 + "px"
+}
+function original() {
+    var e = document.getElementById('div1');
+    e.style.fontSize = 12 + "px"
+}
