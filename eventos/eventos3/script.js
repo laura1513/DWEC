fd105ffbd053b3f8ad5f5c71aae018fe69c32d31
laura1513 <laurajorques2003@gmail.com>
2024-02-01 08:42:08 +0100

    eventos 3

--- eventos/eventos3/script.js
+++ eventos/eventos3/script.js
@@ -2,5 +2,4 @@ function cambiar() {
     const txt = document.getElementById("text").value;
     txt.style.color = 'blue';
 }
-const txt = document.getElementById("text").value;
 document.getElementById("boton").onclick = cambiar;
