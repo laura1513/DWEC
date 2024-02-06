f9bd856efbd95c12bfe57fb7621f47ab15c59c3c
laura <laurajorques2003@gmail.com>
2024-02-06 08:36:57 +0100

    arrays2 3

--- arrays2/array3/script.js
+++ arrays2/array3/script.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    dibujarRombo(7); // Puedes cambiar el número de filas aquí
+    dibujarRombo(7);
 });
 
 function dibujarRombo(filas) {
     let rombo = "";
     let mitad = Math.ceil(filas / 2);
-    
-    // Dibujar la parte superior del rombo
     for (let i = 0; i < mitad; i++) {
         for (let j = 0; j < mitad - i - 1; j++) {
             rombo += "&nbsp;";
@@ -16,8 +14,6 @@ function dibujarRombo(filas) {
         }
         rombo += "<br>";
     }
-    
-    // Dibujar la parte inferior del rombo
     for (let i = mitad - 2; i >= 0; i--) {
         for (let j = 0; j < mitad - i - 1; j++) {
             rombo += "&nbsp;";
