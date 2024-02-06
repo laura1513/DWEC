6e9ee629c81bec74a6077ef24d76231a4f27328c
laura1513 <laurajorques2003@gmail.com>
2024-02-06 08:18:40 +0100

    arrays2 3

---
+++ arrays2/array3/script.js
@@ -0,0 +1,25 @@
+function dibujarRombo(filas) {
+    let rombo = "";
+    let mitad = Math.ceil(filas / 2);
+    for (let i = 0; i < mitad; i++) {
+        for (let j = 0; j < mitad - i - 1; j++) {
+            rombo += "&nbsp;";
+        }
+        for (let k = 0; k < 2 * i + 1; k++) {
+            rombo += "*";
+        }
+        rombo += "<br>";
+    }
+    for (let i = mitad - 2; i >= 0; i--) {
+        for (let j = 0; j < mitad - i - 1; j++) {
+            rombo += "&nbsp;";
+        }
+        for (let k = 0; k < 2 * i + 1; k++) {
+            rombo += "*";
+        }
+        rombo += "<br>";
+    }
+    
+    document.getElementById("rombo").innerHTML = rombo;
+}
+dibujarRombo(7);
