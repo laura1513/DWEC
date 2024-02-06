ee84e3dcb67fef7d02556052c43fcce2080c4322
laura1513 <laurajorques2003@gmail.com>
2024-02-06 08:23:01 +0100

    arrays2 3

--- arrays2/array3/script.js
+++ arrays2/array3/script.js
@@ -1,6 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    dibujarRombo(7); // Puedes cambiar el número de filas aquí
+});
+
 function dibujarRombo(filas) {
     let rombo = "";
     let mitad = Math.ceil(filas / 2);
+    
+    // Dibujar la parte superior del rombo
     for (let i = 0; i < mitad; i++) {
         for (let j = 0; j < mitad - i - 1; j++) {
             rombo += "&nbsp;";
@@ -10,6 +16,8 @@ function dibujarRombo(filas) {
         }
         rombo += "<br>";
     }
+    
+    // Dibujar la parte inferior del rombo
     for (let i = mitad - 2; i >= 0; i--) {
         for (let j = 0; j < mitad - i - 1; j++) {
             rombo += "&nbsp;";
@@ -22,4 +30,3 @@ function dibujarRombo(filas) {
     
     document.getElementById("rombo").innerHTML = rombo;
 }
-dibujarRombo(7);
